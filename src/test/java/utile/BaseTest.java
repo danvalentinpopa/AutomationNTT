6233d5474ee32f798de2f356fdf895ca9ce86dd9
EPOPADA08 <danvalentinpopa@gmail.com>
2024-06-19 09:07:10 +0300

    Removing unused imports

--- src/test/java/utile/BaseTest.java
+++ src/test/java/utile/BaseTest.java
@@ -14,10 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 public class BaseTest {
 
